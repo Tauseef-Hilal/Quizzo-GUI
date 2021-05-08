4c7809ed2671ee5d71c7f3780c42014d70a36407
Tauseef-Hilal <tantary.tauseef@gmail.com>
2021-05-08 11:19:00 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Quizzo (GUI)
+
+## `--About--`
+__A basic GUI based quiz game written in `Python` and `PyQt6`.__
+
+> Written by `Tauseef Hilal Tantary`. *A beginner Python Programmer.*
+
+
+## `--SYSTEM Requirements--`
+* **OS**: Any
+* **RAM**: 15MB
+* ***Python and other dependencies must be installed!***
+
+
+## `--Installation--`
+* Download the repository.
+* Run **`install.py`** to install dependencies.
+
+
+## `--Depends On--`
+*PyQt6==6.0.3*<br>
+*PyQt6-Qt6==6.0.2*<br>
+*PyQt6-sip==13.0.1*<br>
---
+++ install.py
@@ -0,0 +1,22 @@
+"""Install the dependencies"""
+
+import os, sys, time
+
+try:
+    with open('Requirements.txt') as _file:
+        deps = _file.readlines()
+
+        for dep in deps:
+            print(f"Installing {dep[:-1]}")
+            if os.name == "nt":
+                os.system(f"py -m pip install {dep[:-1]} -q")
+            else:
+                os.system(f"pip3 install {dep[:-1]} -q")
+                
+except FileNotFoundError:
+    print("[ERROR] Requirements.txt not found")
+    sys.exit(time.sleep(1))
+
+else:
+    print("Installation Successful!")
+    sys.exit(time.sleep(1))
